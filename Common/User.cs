d9f24bf9e522fea0caa7d51864da91ffb8449aac
Ignacio Tampe <Igtampe@gmail.com>
2022-04-06 18:15:23 -0400

    Make these things properties instead of variables

--- Common/User.cs
+++ Common/User.cs
@@ -19,15 +19,15 @@ namespace Clothespin2.Common {
 
         /// <summary>list of wearables this user owns</summary>
         [JsonIgnore]
-        public List<Wearable> Wearables = new();
+        public List<Wearable> Wearables { get; set; } = new();
 
         /// <summary>List of outfits this user owns</summary>
         [JsonIgnore]
-        public List<Outfit> Outfits = new();
+        public List<Outfit> Outfits { get; set; } = new();
 
         /// <summary>List of logs this user has made </summary>
         [JsonIgnore]
-        public List<LogItem> LogItems = new();
+        public List<LogItem> LogItems { get; set; } = new();
 
         /// <summary>Checks a given password for this user</summary>
         /// <param name="Check"></param>
